13951d7bdb0a1348cfb667685acb895f5bafb22a
Evenstar-E <84828777+Evenstar-E@users.noreply.github.com>
2023-07-19 13:28:14 +0800

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -20,7 +20,7 @@ import tempfile
 st.set_page_config(page_title="ChatPDF",page_icon="🧊",layout="wide")
 
 con = pymysql.connect(
-    host = "127.0.0.1",
+    host = "10.27.123.95",
     port = 3306,
     user = "root",
     password = "2592t03081",
